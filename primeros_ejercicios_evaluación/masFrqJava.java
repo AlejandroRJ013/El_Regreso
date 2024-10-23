7b15c2a3de16612ee605fff6884e12d5fbe818ee
AlejandroRJ013 <alejandro.r.j.tarragona@gmail.com>
2024-10-23 17:22:32 +0200

    Create masFrqJava.java

---
+++ primeros_ejercicios_evaluación/masFrqJava.java
@@ -0,0 +1,11 @@
+package primeros_ejercicios_evaluación;
+
+import java.util.HashMap;
+
+public class masFrqJava {
+    public int contador = 0;
+
+    public static void main(String args[]) {
+        HashMap<Integer, Integer> numCant = new HashMap<>();
+    }
+}
